822e80dff8131e754e754ab49f602c9ab78a0190
Debrena McEwen <debrenamcewen@gmail.com>
2016-11-21 16:28:35 -0800

    This adds images for the links that will lead to each sections page.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/listing', function(req, res, next) {
+  res.render('listing', { title: 'Express' });
+});
+
 module.exports = router;
