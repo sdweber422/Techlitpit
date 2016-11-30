c57ad78e0f7ee7d89f3af6f072dd3638317a1378
sdweber422 <sdweber422@gmail.com>
2016-11-29 18:40:31 -0800

    Fixes listing route, moves functions to db.js and is able to retrieve authors and genres

--- routes/listing.js
+++ routes/listing.js
@@ -7,32 +7,8 @@ router.get('/', function(req, res, next) {
   const page = parseInt(req.query.page || 1)
   const offset = (page - 1) * 10
   return Resources.all(offset)
-  // .then( results => {
-  //   const resources = results
-  //   const resourceIds = results.map( result => result.id )
-  //   const authors = []
-  //   const categories = []
-  //   resourceIds.forEach( id =>
-  //     authors.push(Resources.getAuthors(id)))
-  //   resourceIds.forEach( id =>
-  //     categories.push(Resources.getCategories(id)))
-  //   const writers = Promise.all(authors)
-  //   const genres = Promise.all(categories)
-  //   return Promise.all([writers, genres, resources])
-  // })
-  // .then( result => {
-  //   const authors = result[0]
-  //   const categories = result[1]
-  //   const resources = result[2]
-  //   console.log('authors:  ', Array.forEach(Array.forEach(authors)) );
-  //   resources.forEach( resource => {
-  //     resource.authors = authors.forEach( authoritative => console.log('1'))
-  //     resource.categories = categories.forEach( categorical => categorical.filter( category => category.resource_id === resource.id))
-  //   })
-  //   return resources
-  // })
-  .then( results => {
-    res.render('listing', { title: 'Resource listings', items: results })
+  .then( items => {
+    res.render('listing', { title: 'Resource listings', items: items })
   })
 });
 
